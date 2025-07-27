f80d0bc85d2bef94f369b0a22bbbca535a209136
Iqbal Hassan Mahmud <iqbalmahmud.cs@gmail.com>
2025-07-27 16:26:42 +0600

    zzz

--- Src/MasterInCSharpAndASPDotNET/068Project4StudentManagementSystem/Student.cs
+++ Src/MasterInCSharpAndASPDotNET/068Project4StudentManagementSystem/Student.cs
@@ -4,50 +4,49 @@ using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace _068Project4StudentManagementSystem
 {
     public class Student
     {
-        private string? _name;
-        private DateTime? _dateOfBirth;
-        private string? _rollNumber;
-
-        public string? Name { get { return _name; } set { _name = value; } }
+        public string? Name { get; set; }
         public DateTime? DateOfBirth 
-        { 
-            get { return _dateOfBirth; } 
-            set 
-            { 
-                if (value > DateTime.Now)
-                {
-                    throw new ArgumentException("Date of birth can't be in the future.");
-                }
-                _dateOfBirth = value; 
-            } 
+        {
+            get;
+            private set;
         }
-        public int? Age { get { return CalculateAge(); } }
-        public string? RollNumber { get { return _rollNumber; } set{ _rollNumber = value;} }
+        public int? Age => CalculateAge();  //public int? Age { get { return CalculateAge(); } }
+        public string? RollNumber { get; private set; }
 
         public Student() { }
 
-        public Student(string name, DateTime dateofBirth, string rollNumber) 
+        public Student(string name, DateTime dateOfBirth, string rollNumber) 
         {
+            ValidateInput(name, dateOfBirth, rollNumber);
             Name = name;
-            DateOfBirth = dateofBirth;
+            DateOfBirth = dateOfBirth;
             RollNumber = rollNumber;
+        }
+
+        private static void ValidateInput(string name, DateTime dateOfBirth, string rollNumber)
+        {
+            if (dateOfBirth > DateTime.Now)
+                throw new ArgumentException("Date of birth can't be in the future!");
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentNullException("Name can't be null or empty!");
+            if (string.IsNullOrWhiteSpace(rollNumber))
+                throw new ArgumentException("Roll Number can't be null or empty!");
+            if (dateOfBirth == default)
+                throw new ArgumentException("Date of birth can't be null!");
         }
 
         private int CalculateAge()
         {
-            if (_rollNumber == null)
-                return 0;
-
             int age = DateTime.Now.Year - DateOfBirth.Value.Year;
-            return age;
+
+            return DateTime.Now < DateOfBirth.Value.AddYears(age) ? age-- : age;
         }
-
-
 
         public void DisplayInformation()
         {
